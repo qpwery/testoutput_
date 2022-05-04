4aed3c55df29e4ea8e20cf0e9dbb7abb4869669b
qpwery <101112062+qpwery@users.noreply.github.com>
2022-05-04 11:11:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setuptools.setup(
   description'oss-dev my output 2143',
   author='jieunkim',
   url="https://canvas.donga.ac.kr/courses/50093/external_tools/2",
-  download_url="",
   install_requires=['pytest'],
   long_description='oss-dev testoutput python module',
   long_description_content_type='text/markdown',
