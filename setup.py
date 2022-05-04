94af0b48b83e93c7c97a6b11316e4d8e16653467
qpwery <101112062+qpwery@users.noreply.github.com>
2022-05-04 11:08:17 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True,
+  name="mytestoutput2143',
+  version='0.0.1',
+  description'oss-dev my output 2143',
+  author='jieunkim',
+  url="https://canvas.donga.ac.kr/courses/50093/external_tools/2",
+  download_url="",
+  install_requires=['pytest'],
+  long_description='oss-dev testoutput python module',
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
